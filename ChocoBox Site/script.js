bf16721c6d29d7dda371f8dfbc5851a682d73c5c
GuinaF2 <guifo2.henrique@gmail.com>
2025-04-08 14:10:27 -0300

    Ajuste Carrossel Pagina Irlanda

--- ChocoBox Site/script.js
+++ ChocoBox Site/script.js
@@ -21,26 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector(".container_gastronomia");
-    const images = document.querySelectorAll(".container_gastronomia img");
-    let index = 0;
-
-    function moveCarousel() {
-        index++;
-        if (index >= images.length) {
-            index = 0;
-        }
-        const offset = -index * container.clientWidth;
-        container.style.transform = `translateX(${offset}px)`;
-    }
-
-    setInterval(moveCarousel, 3000); // Troca de imagem a cada 3 segundos
-});
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const images = document.querySelectorAll(".carrossel1 img");
+    const images = document.querySelectorAll(".carrossel-irlanda img");
     let currentIndex = 0;
 
     function showNextImage() {
@@ -54,30 +35,12 @@ document.addEventListener("DOMContentLoaded", () => {
         images[currentIndex].classList.add("active");
     }
 
-    // Inicializa a primeira imagem como ativa
-    images[currentIndex].classList.add("active");
-
     // Define o intervalo para alternar as imagens a cada 3 segundos
     setInterval(showNextImage, 3000);
+
+    // Inicializa a primeira imagem como ativa
+    images[currentIndex].classList.add("active");
 });
 
-
-//*carrossel1 *//
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector(".container");
-    const images = document.querySelectorAll(".container img");
-    let index = 0;
-
-    function moveCarousel() {
-        index++;
-        if (index >= images.length) {
-            index = 0;
-        }
-        const offset = -index * 100; // Move 100% para cada imagem
-        container.style.transform = `translateX(${offset}%)`;
-    }
-
-    setInterval(moveCarousel, 3000); // Troca de imagem a cada 3 segundos
-});
 
 //* nao vai ahhh**//
